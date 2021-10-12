cb2bbeba3bce93a5422ec276d2ad59022a4fa782
Burdyuja Alexandr <leomessi-2003-23@mail.ru>
2021-10-12 19:35:58 +0300

    added task3

---
+++ HW/homework/HW/task3/task3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+#ifndef __cplusplus
+#define bool int
+#define true 1
+#define false 0
+#endif
+
+int *user_n, *pc_n, num;
+
+
+int num_generate(int n) {
+	int num = 0, i = 0;
+	int numbers[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	srand(time(0));
+
+	while (n > 0) {
+		i = rand() % 10 + 1;
+		while ((numbers[i] < 0) && (i < 10)) 
+			i++;
+		while ((numbers[i] < 0) && (i > -1))
+			i--;
+		num = num * 10 + numbers[i];
+		numbers[i] = -1;
+		n--;
+	}
+
+	return num;
+}
+
+void c_and_b(int *pc_num, int *user_num, int n, int *c_count, int *b_count) {
+
+	*c_count = *b_count = 0;
+
+	for (int i = 0; i < n; i++) {
+
+		if ((pc_n[i] == user_n[i]) && (user_n[i] > -1)) {
+			*b_count += 1;
+			user_n[i] = -1;
+		}
+
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((pc_n[i] == user_n[j]) && (user_n[j] > -1)) {
+				*c_count += 1;
+				user_n[j] = -1;
+			}
+		}
+	}
+}
+
+
+
+int main() {
+
+	int n, user_num, c = 0, b = 0;
+
+	while (true) {
+		printf("vvedite n(ot 2 do 5): \n");
+		scanf_s("%d", &n);
+		if ((n >= 2) && (n <= 5))
+			break;
+		else
+			printf("nepravilnii diaposon\n");
+	}
+
+	user_n = (int)malloc(n * sizeof(int));
+	pc_n = (int)malloc(n * sizeof(int));
+	num = num_generate(n);
+
+	int i = 0;
+
+	while (num != 0) {
+		pc_n[i] = num % 10;
+
+		num /= 10;
+		i++;
+	}
+
+
+	while (true) {
+		printf("vvedite chislo bez povtorenii: ");
+		scanf_s("%d", &user_num);
+
+		i = 0;
+
+
+		while (user_num != 0) {
+			user_n[i] = user_num % 10;
+			user_num /= 10;
+			i++;
+		}
+
+		c_and_b(pc_n, user_n, n, &c, &b);
+
+		if (b == n) {
+			printf("vi ugadali chislo!");
+			break;
+		}
+		else
+			printf("kol-vo c = %d\n kol-vo b = %d\n", c, b);
+	}
+
+
+	return 0;
+}
